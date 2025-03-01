63233646d2d584d0c5d0276360f6d94f251431cd
Yuki Osawa <y.osawa.cingroup@gmail.com>
2025-03-01 23:37:24 +0900

    fast　commit

---
+++ src/basicSyntax/DataType.java
@@ -0,0 +1,15 @@
+package basicSyntax;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("こんにちは"+"私は"+"Gee君です。");
+		System.out.println(10);
+		System.out.println("20"+"24");
+		System.out.println(3.14);
+		System.out.println(true);
+		
+	}
+
+}
