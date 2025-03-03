e74c1cac84ad640f75a12d2fa296aa3c776520c8
Yuki Osawa <y.osawa.cingroup@gmail.com>
2025-03-03 14:53:51 +0900

    コミット

---
+++ src/basicSyntax/Main.java
@@ -0,0 +1,15 @@
+package basicSyntax;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("こんにちは");
+		System.out.println("私の名前は");
+		System.out.println("Gee君です。");
+		System.out.println("こんにちは"+"私の名前は"+"Gee君です。");
+		
+
+	}
+
+}
