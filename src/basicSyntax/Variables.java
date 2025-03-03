4744fd98bf9c2b635f66d12b30735982402597c8
Yuki Osawa <y.osawa.cingroup@gmail.com>
2025-03-03 14:38:35 +0900

    fast commit

---
+++ src/basicSyntax/Variables.java
@@ -0,0 +1,18 @@
+package basicSyntax;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int num1 = 7;
+		double num2 = 16;
+		String I = "私の";
+		String like = "好きな";
+				
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(I+like+"整数は"+num1+"です。");
+		System.out.println(I+like+"小数は"+num2+"です。");
+	}
+
+}
