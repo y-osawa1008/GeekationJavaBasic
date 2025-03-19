2100ef2d68009c39ff13b33f54183ea62985d347
Yuki Osawa <y.osawa.cingroup@gmail.com>
2025-03-19 17:10:47 +0900

    commit

--- src/basicSyntax/Main.java
+++ src/basicSyntax/Main.java
@@ -4,12 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("こんにちは");
-		System.out.println("私の名前は");
-		System.out.println("Gee君です。");
-		System.out.println("こんにちは"+"私の名前は"+"Gee君です。");
+		GeekIntroduction.greeting(27, 170);
+		GeekIntroduction.specialSkill(3, 8);
 		
-
 	}
 
 }
